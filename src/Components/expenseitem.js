b917eca648b453effd2f7114a8afd17a68cde1bb
PrajjwalKhatri <prajjwal.khatri@meetribbon.com>
2021-04-22 18:01:03 +0530

    22/04/2021 Section 3

--- src/Components/expenseitem.js
+++ src/Components/expenseitem.js
@@ -1,5 +1,20 @@
-function ExpenseItem() {
-    return <h2>Expense Item! </h2>
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const expenseDate = new Date(2021, 3, 22);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 500.90;
+   
+    return (
+    <div className="expense-item">
+        <div>{props.date.toISOString()}</div>
+    <div className="expene-item__description">
+        <h2>{props.title}</h2>
+        </div>
+        <div className="expense-item__price">$ {props.amount}</div>
+    </div>   
+     
+    );
 }
 
 export default ExpenseItem;
